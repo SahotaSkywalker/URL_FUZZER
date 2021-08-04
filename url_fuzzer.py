ab7fd33e78c475bec95ea31594697b4722c101f9
SahotaSkywalker <88429930+SahotaSkywalker@users.noreply.github.com>
2021-08-04 18:04:45 +0800

    Add files via upload

---
+++ url_fuzzer.py
@@ -0,0 +1,19 @@
+import requests
+from requests import status_codes
+import time
+url = input("Pls input your url here:")
+wordlists = input("input wordlist here:")
+slsh =  '/'
+word = open(wordlists,"r")
+line = word.readlines()
+
+for a in line:
+    b = a.rstrip('\n')
+    r = requests.get(url+slsh+b)
+    if r.status_code !=404:
+        print(url+slsh+b+"--"+str(r.status_code),end="\n")
+    
+
+
+
+
